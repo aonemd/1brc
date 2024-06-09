0bea46f1d380f37509a2eb152dbe705ea645f50d
Ahmed Saleh <ahmed.saleh@recharge.com>
2024-06-09 16:04:08 +0200

    Init

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use core::cmp::min;
 use rustc_hash::FxHashMap;
 use std::collections::HashMap;
-use std::io::{prelude::*, BufReader};
+use std::io::{prelude::*, BufReader, Cursor, SeekFrom};
+use std::os::unix::fs::FileExt;
 use std::sync::mpsc::{channel, Receiver, Sender};
+use std::thread::sleep;
+use std::time::Duration;
 
 #[derive(Debug)]
 struct City {
@@ -25,8 +28,8 @@ impl City {
     }
 }
 
+#[inline]
 fn fast_parse_float_to_int(data: &[u8]) -> i32 {
-    let mut point = false;
     let mut negative = false;
     let mut result = 0;
 
@@ -37,7 +40,6 @@ fn fast_parse_float_to_int(data: &[u8]) -> i32 {
         }
 
         if byte == b'.' {
-            point = true;
             continue;
         }
 
@@ -100,81 +102,154 @@ fn main() {
     let mut map: FxHashMap<String, City> = FxHashMap::default();
 
     let path = "./data/measurements.txt";
-    let path = "./data/measurements_100_000_000.txt";
-    // let path = "./data/measurements_10.txt";
+    // let path = "./data/measurements_100_000_000.txt";
+    // let path = "./data/measurements_1000_000.txt";
+    let path = "./data/measurements_10_000_000.txt";
+    let path = "./data/measurements_10.txt";
 
-    let file = std::fs::File::open(path).expect("Failed to read file");
-    let mut reader = BufReader::new(file);
+    // let file = std::fs::File::open(path).expect("Failed to read file");
+    // let mut reader = BufReader::new(file);
     const BLOCK_SIZE: usize = 16 * 1024 * 1024; // 2_097_152; //2M
-    let mut buffer = vec![0_u8; BLOCK_SIZE];
+                                                // let mut buffer = vec![0_u8; BLOCK_SIZE];
     let mut left_over_bytes: Vec<u8> = vec![];
 
-    let (sender, receiver): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = channel();
+    // let (sender, receiver): (Sender<(Vec<u8>, Vec<u8>)>, Receiver<(Vec<u8>, Vec<u8>)>) = channel();
 
-    std::thread::spawn(move || loop {
-        let count = reader.read(&mut buffer).unwrap();
-        if count == 0 {
-            break;
-        }
+    let thread_count = 8;
+    let mut pool = vec![];
 
-        left_over_bytes.extend_from_slice(&buffer);
+    for i in 0..thread_count {
+        let start: u64 = (i * BLOCK_SIZE) as u64;
+        let end: u64 = start + BLOCK_SIZE as u64;
 
-        sender.send(left_over_bytes.clone()).unwrap();
+        let thr = std::thread::spawn(move || {
+            let file = std::fs::File::open(path).expect("Failed to read file");
+            let mut reader = BufReader::new(file);
+            // let mut reader = Cursor::new(&file[start..end]);
+            reader.seek(SeekFrom::Start(start));
 
-        if let Some(last_newline_index) = buffer.iter().rposition(|&b| b == b'\n') {
-            left_over_bytes = buffer[last_newline_index + 1..].to_vec();
-        }
-    });
-
-    for line_data in receiver {
-        let mut start = 0;
-        let mut name_end = 0;
-        for (i, &byte) in line_data.iter().enumerate() {
-            if byte == b';' {
-                name_end = i;
-            }
-
-            if byte == b'\n' && start < name_end {
-                // includes the newline character but the slicing does not since it's
-                // non-inclusive
-                let end = i;
-
-                let name = unsafe { std::str::from_utf8_unchecked(&line_data[start..name_end]) };
-                let temp = fast_parse_float_to_int(&line_data[name_end + 1..end]);
-
-                if map.contains_key(name) {
-                    let city = map.get_mut(name).unwrap();
-                    city.max = (city.max).max(temp);
-                    city.min = (city.max).min(temp);
-                    city.sum += temp;
-                    city.count += 1;
-                } else {
-                    map.insert(name.to_string(), City::new(name.to_string(), temp, temp));
+            let mut buf: [u8; 1] = [0];
+            if i > 0 {
+                // Adjust reader to start at the beginning of a new line
+                loop {
+                    if reader.read_exact(&mut buf).is_err() || buf[0] == b'\n' {
+                        break;
+                    }
                 }
-
-                start = end + 1;
-                name_end = 0;
             }
-        }
+
+            let mut buffer = vec![0_u8; BLOCK_SIZE];
+            let count = reader.read_exact(&mut buffer).unwrap();
+            // if count == 0 {
+            //     break;
+            // }
+
+            let mut buf = vec![0_u8; 8];
+            let count = reader.read_until(b'\n', &mut buf).unwrap();
+            if count == 0 {
+                buf = vec![];
+            }
+
+            let line_data = [buffer, buf].concat();
+
+            println!("{:?}", std::str::from_utf8(&line_data));
+
+            // sender.send((buffer, buf)).unwrap();
+            // if count == 0 {
+            //     let buf_to_send = vec![];
+            //     println!(
+            //         "count is ZERO {:?} {:?}",
+            //         buf_to_send.len(),
+            //         std::str::from_utf8(&buf_to_send)
+            //     );
+            //     sender.send((buffer.clone(), buf_to_send)).unwrap();
+            // } else {
+            //     sender.send((buffer.clone(), buf)).unwrap();
+            // }
+
+            // left_over_bytes.extend_from_slice(&buffer);
+
+            // sender.send(left_over_bytes.clone()).unwrap();
+            // buffer.extend_from_slice(&buf);
+            // combine two slices
+            // let mut combined = Vec::with_capacity(left_over_bytes.len() + buffer.len());
+            // buffer.append(&mut buf);
+
+            // if let Some(last_newline_index) = buffer.iter().rposition(|&b| b == b'\n') {
+            //     left_over_bytes = buffer[last_newline_index + 1..].to_vec();
+            // }
+        });
+
+        pool.push(thr);
     }
 
-    let mut cities = map.into_values().collect::<Vec<City>>();
-    cities.sort_by(|a, b| a.name.cmp(&b.name));
-
-    let mut city_strings = Vec::new();
-    for city in &cities {
-        let city_string = format!(
-            "{}={:.1}/{:.1}/{:.1}",
-            city.name,
-            (city.min as f32 / 10.0),
-            ((city.sum / city.count) as f32 / 10.0).ceil(),
-            (city.max as f32 / 10.0),
-        );
-        city_strings.push(city_string);
+    for thr in pool {
+        thr.join().unwrap();
     }
 
-    let output = format!("{{{}}}", city_strings.join(", "));
-    println!("{}", output);
+    // for line_data in receiver {
+    //     let mut line_data = [line_data.0, line_data.1].concat();
+    //
+    //     // println!("{:?}", std::str::from_utf8(&line_data));
+    //     // println!("{:?}", line_data.len());
+    //
+    //     let mut start = 0;
+    //     let mut name_end = 0;
+    //     for (i, &byte) in line_data.iter().enumerate() {
+    //         if byte == b';' {
+    //             name_end = i;
+    //         }
+    //
+    //         if byte == b'\n' && start < name_end {
+    //             // includes the newline character but the slicing does not since it's
+    //             // non-inclusive
+    //             let end = i;
+    //
+    //             // let name = std::str::from_utf8(&line_data[start..name_end]).unwrap();
+    //             let name = unsafe { std::str::from_utf8_unchecked(&line_data[start..name_end]) };
+    //             let temp = fast_parse_float_to_int(&line_data[name_end + 1..end]);
+    //
+    //             // let city =
+    //             //     map.entry(name.to_string())
+    //             //         .or_insert(City::new(name.to_string(), temp, temp));
+    //             // city.max = (city.max).max(temp);
+    //             // city.min = (city.max).min(temp);
+    //             // city.sum += temp;
+    //             // city.count += 1;
+    //
+    //             if map.contains_key(name) {
+    //                 let city = map.get_mut(name).unwrap();
+    //                 city.max = (city.max).max(temp);
+    //                 city.min = (city.max).min(temp);
+    //                 city.sum += temp;
+    //                 city.count += 1;
+    //             } else {
+    //                 map.insert(name.to_string(), City::new(name.to_string(), temp, temp));
+    //             }
+    //
+    //             start = end + 1;
+    //             name_end = 0;
+    //         }
+    //     }
+    // }
+    //
+    // let mut cities = map.into_values().collect::<Vec<City>>();
+    // cities.sort_by(|a, b| a.name.cmp(&b.name));
+    //
+    // let mut city_strings = Vec::new();
+    // for city in &cities {
+    //     let city_string = format!(
+    //         "{}={:.1}/{:.1}/{:.1}",
+    //         city.name,
+    //         (city.min as f32 / 10.0),
+    //         ((city.sum / city.count) as f32 / 10.0).ceil(),
+    //         (city.max as f32 / 10.0),
+    //     );
+    //     city_strings.push(city_string);
+    // }
+    //
+    // let output = format!("{{{}}}", city_strings.join(", "));
+    // println!("{}", output);
 }
 
 #[cfg(test)]
